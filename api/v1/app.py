7330c5b6395caec4ff7ca52950f5db1bcbc296de
amine784 <1066@holbertonschool.com>
2020-04-29 09:08:50 +0100

    holberto

---
+++ api/v1/app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+'''first step to create rest api'''
+from models import storage
+from flask import Flask
+from api.v1.views import app_views
+from flask import jsonify
+from os import getenv
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def close_session(exep):
+    '''close session'''
+    storage.close()
+if __name__ == "__main__":
+    HOST = getenv('HBNB_API_HOST')
+    PORT = getenv('HBNB_API_PORT')
+    app.run(host=HOST, port=PORT, threaded=True)
